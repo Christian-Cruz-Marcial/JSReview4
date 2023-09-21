b67d1a2515d18aa49edbb14f8e587e1dd7312531
Christian Cruz <c.cruzmarcial2001@gmail.com>
2023-09-21 13:44:02 -0500

    finished

--- main.js
+++ main.js
@@ -1,103 +1,79 @@
-//Christian Cruz Marcial
-// excercise 1
-function outerFunction() {
-	let outerVar = "I'm outside!";
-
-	function innerFunction() {
-		let innerVar = "I'm inside!";
-		console.log(outerVar); // this uses inbeded scope
-		console.log(innerVar); // this just uses block scope
-	}
-    
-
-	innerFunction();
+//9/18/23
+//excercise 1
+function test(){
+	console.log(this);
 }
 
-outerFunction();
-
+const food = {
+	fruit: "apple",
+	vegi: "corn",
+	meat: "chicken",
+	meal: function() {
+		const arrowFuction = ()=>{
+		console.log(this)
+		console.log(this.fruit + ", " + this.vegi + ", " + this.meat)
+		}
+		arrowFuction();
+	}
+}
+test();
+food.meal()
 //excercise 2
-function mainFunction() {
-	hoistedFunction();
-	function hoistedFunction(){
-        console.log("Im Hoisted!")
-    }
-}
-
-mainFunction();
-
-//excercise 3
-function closure(){
-    const name = 'Christian'
-    return function returnClosure(){
-        console.log(name);
-    }
-}
-//excercise 4
-let globe = 'Earth';
-modGlobe();
-function modGlobe(){
-	let globe = "Mars";
-	console.log(globe);
-}
-changeGlobe();
-function changeGlobe(){
-	let local = 1;
-	// let local = 5;
-	console.log(local)
-}
-
-//excercise 5
-function find(x){
-	result = x*2;
-	console.log(result)
-}
-find(6);
-
-//excercise 6
-function hoistTest(){
-	let Paladin = "Chad";
-	console.log(Paladin);
-	//let Paladin = "sucks"
-}
-hoistTest();
-
-//excercise 7
-function counter(x){
-	return function countUp(){
-		let y = ++x;
-		console.log(y)
-		
+class person {
+	constructor(name,age){
+		this.name = name,
+		this.age = age
 	}
-}
-const End = counter(0);
-End();
-End();
-End();
-End();
-
-//excercise 8
-function count(num){
-	let count = num;
-	return function(){
-		const incr = ++count;
-		if (incr >= 5){
-			console.log('tired of counting now')
+	greet(phrase){
+		console.log(`${this.name} says:  ${phrase} ${this.age}`)
+	}
+	//excercise 4
+	static info(){
+		console.log('this is in the person class')
+	}	
+	//excercise 5
+	get current(){
+		return age;
+	}
+	set current(age){
+		if ( age <= 0){
+		console.log('negative number');
 		} else {
-			console.log(incr);
+		age = this.age;
+		console.log(age);
 		}
 	}
 }
-let lazyCount = count();
-lazyCount();
-lazyCount();
-lazyCount();
-//excercise 9
-function CarFactory(cars){
-	let kia = cars;
-	console.log(kia)
-	return function(bike){
-		let kia = bike;
-		console.log(kia);
+//console.log(person.info)
+//shows line 31 in console
+const christian = new person('christian', 21);
+
+
+//console.log(christian.info)
+//prints undefined
+const trez = new person('trez', 24);
+trez.greet("Hi, im")
+//excercise 3
+const v = new person('V', 18);
+v.greet('Hi, im')
+const kayden = new person('Kayden', 17);
+kayden.greet('Hi, im')
+
+//excercise 7
+class student{
+	constructor(name, grade){
+		this.name = name,
+		this.grade = grade;
+	}
+	get average(){
+		return grade
+	}
+	set average(grade){
+		this.log.push(grade)
+		const averageGrade = array => array.reduce((a, b) => a + b) / array.length;
+		console.log(averageGrade);
 	}
 }
-CarFactory();
+const jim = new student('jim', 89);
+const tim = new student('tim', 95);
+const kim = new student('kim', 76);
